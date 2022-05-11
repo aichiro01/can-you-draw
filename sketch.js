0ea0e931eb1059d1120bc7de4f6f0994f9e83842
aichiro01 <aichiro01@outlook.com>
2022-05-11 16:50:14 -0700

    change

--- sketch.js
+++ sketch.js
@@ -1,61 +1,27 @@
 let gamestate = "title";
 
-let drawStart = false;
-
-let countDown;
-
-let goR;
-
-let goG;
-
-let goB;
-
-let goFill;
-
-let goText;
-
-let pic0;
-
-let pic1;
-
-let pic2;
-
-let pic3;
-
-
-
-function preload(){
-  pic0 = loadImage('assets/0.png');
-  pic1 = loadImage('assets/1.png');
-  pic2 = loadImage('assets/2.png');
-  pic3 = loadImage('assets/3.png');
-}
 
 function setup() {
-  createCanvas(500, 500);
-  background(255);
+  createCanvas(800, 800);
 
-  frameRate(80);
+  background(255, 255, 255);
 
-  myButton = new Clickable(); //Create button
+    myButton = new Clickable();
 
   myButton.onPress = function () {
-    //When myButton is pressed
-    this.color = "#00FF00"; //Change button color
-    alert("Shall we begin?"); //Show an alert message
+    this.color = "#00FF00";
   };
-  myButton.width = 250;
-  myButton.height = 100;
-  myButton.locate(125, 250); //Position Button
-  myButton.text = "Play";       //Text of the clickable (string)
-  myButton.color = "#00FF00";       //Background color of the clickable (hex number as a string)
-  myButton.textSize = 30;           //Size of the text (integer)
-myButton.textFont = "monospace"; //Font of the text (string)
+  myButton.width = 400;
+  myButton.height = 200;
+  myButton.locate(200, 500);
+  myButton.text = "Play";
+  myButton.color = "#00FF00";
+  myButton.textSize = 70;
+myButton.textFont = "monospace";
 
   myButton.onPress = function(){
    gamestate = "info";
 }
-
 
 }
 
@@ -96,6 +62,7 @@ function draw() {
       break;
       case 'gameover':
       gameOver();
+
   }
 
   myButton.draw();
@@ -105,162 +72,160 @@ function draw() {
     stroke(5);
    line(mouseX, mouseY, pmouseX, pmouseY);
   }
+
 }
 
-
-
-function titleScreen() {
+function titleScreen(){
   noStroke();
-   fill(58, 58, 58);
-   rect(10, 20, 485, 475);
+   fill(255);
+   rect(0, 0, 800, 800);
    fill(245, 245, 220);
    strokeWeight(1);
-   rect(0, 0, 500, 40, 20);
-   rect(0, 460, 500, 40, 20);
-   rect(0, 0, 20, 500, 20);
-   rect(480, 0, 20, 500, 20);
+   rect(0, 0, 800, 40, 20);
+   rect(0, 760, 800, 40, 20);
+   rect(0, 0, 20, 800, 20);
+   rect(780, 0, 20, 800, 20);
    stroke(255);
-   fill(255);
-   textSize(50);
+   fill(0);
+   textSize(80);
    textAlign(CENTER);
    text('Can you Draw?', width*0.5, height*0.35);
+   fill(245, 300, 220);
+   textSize(40);
+   textAlign(CENTER);
+   text('Dell', width*0.5, height*.99);
    fill(0, 255, 0);
-   ellipse(400, 480, 20, 20);
+   ellipse(700, 780, 20, 20);
    fill(245, 245, 220);
    stroke(1);
-   ellipse(450, 480, 30, 30);
+   ellipse(750, 780, 30, 30);
    fill(58, 58, 58);
-   ellipse(450, 480, 10, 15);
+   ellipse(750, 780, 10, 15);
+
 }
 
 function intro() {
   noStroke();
-   fill(58, 58, 58);
-   rect(10, 20, 485, 475);
+   fill(255);
+   rect(0, 0, 800, 800);
    fill(245, 245, 220);
    strokeWeight(1);
-   rect(0, 0, 500, 40, 20);
-   rect(0, 460, 500, 40, 20);
-   rect(0, 0, 20, 500, 20);
-   rect(480, 0, 20, 500, 20);
+   rect(0, 0, 800, 40);
+   rect(0, 760, 800, 40);
+   rect(0, 0, 20, 800);
+   rect(780, 0, 20, 800);
    stroke(255);
-   fill(255);
+   fill(0);
    textAlign(CENTER);
    textSize(35);
-   text('Info:', width*0.5, height*0.18);
+   text('Info:', width*0.5, height*0.1);
    textSize(20);
    textSize(22);
-   text('Replicate each image to the best of your ability', width*0.5, height*0.3);
-   text('You can only do it in one line', width*0.5, height*0.4);
-   text('Clear canvas per drawing', width*0.5, height*0.5);
+   text('Replicate each image to the best of your ability', width*0.5, height*0.2);
+   text('You can only do it in one line', width*0.5, height*0.3);
+   text('Clear canvas per drawing', width*0.5, height*0.4);
    textSize(35);
-   text('Controls:', width*0.5, height*0.65);
+   text('Controls:', width*0.5, height*0.75);
    textSize(22);
-   text('Press "c" to clear canvas', width*0.5, height*0.75);
+   text('Press "c" to clear canvas', width*0.5, height*0.85);
+   fill(245, 300, 220);
+   textSize(40);
+   textAlign(CENTER);
+   text('Dell', width*0.5, height*.99);
    fill(0, 255, 0);
-   ellipse(400, 480, 20, 20);
+   ellipse(700, 780, 20, 20);
    fill(245, 245, 220);
    stroke(1);
-   ellipse(450, 480, 30, 30);
+   ellipse(750, 780, 30, 30);
    fill(58, 58, 58);
-   ellipse(450, 480, 10, 15);
-   myButton.width = 90;
-   myButton.height = 70;
-   myButton.locate(380, 380);
-   myButton.text = "Next";
+   ellipse(750, 780, 10, 15);
+   myButton.width = 70;
+   myButton.height = 50;
+   myButton.locate(width*0.87, height*0.88);
+   myButton.text = "Begin";
    myButton.color = "#00FF00";
-   myButton.textSize = 30;
+   myButton.textSize = 20;
    myButton.textFont = "monospace";
    myButton.onPress = function(){
     gamestate = "lvl1";
  }
 }
 
-function play() {
-
+function play(){
   noStroke();
-  fill(245, 245, 220)
-  rect(0, 0, 500, 40, 20);
-  rect(0, 460, 500, 40, 20);
-  rect(0, 0, 20, 500, 20);
-  rect(480, 0, 20, 500, 20);
   fill(58);
-  rect(20, 40, 200, 420);
-  rect(20, 40, 440, 150);
-  rect(460, 40, 20, 420);
-  rect(20, 372, 460, 90);
+  rect(20, 40, 190, 790);
+  rect(20, 40, 790, 300);
+  rect(600, 40, 200, 800);
+  rect(20, 550, 800, 700);
+  fill(245, 300, 220);
+  fill(245, 245, 220);
+  strokeWeight(1);
+  rect(0, 0, 800, 40);
+  rect(0, 760, 800, 40);
+  rect(0, 0, 20, 800);
+  rect(780, 0, 20, 800);
+  textSize(40);
+  textAlign(CENTER);
+  text('Dell', width*0.5, height*.99);
   fill(0, 255, 0);
-  ellipse(400, 480, 20, 20);
+  ellipse(700, 780, 20, 20);
   fill(245, 245, 220);
   stroke(1);
-  ellipse(450, 480, 30, 30);
+  ellipse(750, 780, 30, 30);
   fill(58, 58, 58);
-  ellipse(450, 480, 10, 15);
-
-  image(pic0,45, 197);
-
+  ellipse(750, 780, 10, 15);
   fill(255, 255, 255);
   textAlign(CENTER);
   textSize(25);
-  text('Can you Draw:', width*0.5, height*0.2);
+  text('Can you Draw:', width*0.5, height*0.1);
   textSize(20);
-  text('Tree', width*0.5, height*0.3);
-
-  if (drawStart == false){
-    background(255,255,255);
-    drawStart = true;
-  }
-  myButton.width = 90;
-  myButton.height = 70;
-  myButton.locate(380, 380);
-  myButton.text = "Lvl2";
-  myButton.color = "#00FF00";
-  myButton.textSize = 30;
-  myButton.textFont = "monospace";
-  myButton.onPress = function(){
-   gamestate = "lvl2";
-}
-
+  text('Tree', width*0.5, height*0.16);
+   myButton.width = 200;
+   myButton.height = 70;
+   myButton.locate(width*0.38, height*0.8);
+   myButton.text = "Lvl2";
+   myButton.color = "#00FF00";
+   myButton.textSize = 30;
+   myButton.textFont = "monospace";
+   myButton.onPress = function(){
+    gamestate = "lvl2";
+    }
 }
 
 function play1() {
-
   noStroke();
-  fill(245, 245, 220)
-  rect(0, 0, 500, 40, 20);
-  rect(0, 460, 500, 40, 20);
-  rect(0, 0, 20, 500, 20);
-  rect(480, 0, 20, 500, 20);
   fill(58);
-  rect(20, 40, 200, 420);
-  rect(20, 40, 440, 150);
-  rect(460, 40, 20, 420);
-  rect(20, 372, 460, 90);
+  rect(20, 40, 190, 790);
+  rect(20, 40, 790, 300);
+  rect(600, 40, 200, 800);
+  rect(20, 550, 800, 700);
+  fill(245, 300, 220);
+  fill(245, 245, 220);
+  strokeWeight(1);
+  rect(0, 0, 800, 40);
+  rect(0, 760, 800, 40);
+  rect(0, 0, 20, 800);
+  rect(780, 0, 20, 800);
+  textSize(40);
+  textAlign(CENTER);
+  text('Dell', width*0.5, height*.99);
   fill(0, 255, 0);
-  ellipse(400, 480, 20, 20);
+  ellipse(700, 780, 20, 20);
   fill(245, 245, 220);
   stroke(1);
-  ellipse(450, 480, 30, 30);
+  ellipse(750, 780, 30, 30);
   fill(58, 58, 58);
-  ellipse(450, 480, 10, 15);
-
-  image(pic0,45, 197);
-
+  ellipse(750, 780, 10, 15);
   fill(255, 255, 255);
   textAlign(CENTER);
   textSize(25);
-  text('Can you Draw:', width*0.5, height*0.2);
+  text('Can you Draw:', width*0.5, height*0.1);
   textSize(20);
-  text('Fish', width*0.5, height*0.3);
-
-  if (drawStart == false){
-    background(255, 255, 255);
-    drawStart = true;
-  }
-
-  myButton.width = 90;
+  text('Fish', width*0.5, height*0.16);
+  myButton.width = 200;
   myButton.height = 70;
-  myButton.locate(380, 380);
+  myButton.locate(width*0.38, height*0.8);
   myButton.text = "Lvl3";
   myButton.color = "#00FF00";
   myButton.textSize = 30;
@@ -268,46 +233,44 @@ function play1() {
   myButton.onPress = function(){
    gamestate = "lvl3";
 }
-}
+  }
+
+
+
 
 function play2() {
-
   noStroke();
-  fill(245, 245, 220)
-  rect(0, 0, 500, 40, 20);
-  rect(0, 460, 500, 40, 20);
-  rect(0, 0, 20, 500, 20);
-  rect(480, 0, 20, 500, 20);
   fill(58);
-  rect(20, 40, 200, 420);
-  rect(20, 40, 440, 150);
-  rect(460, 40, 20, 420);
-  rect(20, 372, 460, 90);
+  rect(20, 40, 190, 790);
+  rect(20, 40, 790, 300);
+  rect(600, 40, 200, 800);
+  rect(20, 550, 800, 700);
+  fill(245, 300, 220);
+  fill(245, 245, 220);
+  strokeWeight(1);
+  rect(0, 0, 800, 40);
+  rect(0, 760, 800, 40);
+  rect(0, 0, 20, 800);
+  rect(780, 0, 20, 800);
+  textSize(40);
+  textAlign(CENTER);
+  text('Dell', width*0.5, height*.99);
   fill(0, 255, 0);
-  ellipse(400, 480, 20, 20);
+  ellipse(700, 780, 20, 20);
   fill(245, 245, 220);
   stroke(1);
-  ellipse(450, 480, 30, 30);
+  ellipse(750, 780, 30, 30);
   fill(58, 58, 58);
-  ellipse(450, 480, 10, 15);
-
-  image(pic0,45, 197);
-
+  ellipse(750, 780, 10, 15);
   fill(255, 255, 255);
   textAlign(CENTER);
   textSize(25);
-  text('Can you Draw:', width*0.5, height*0.2);
+  text('Can you Draw:', width*0.5, height*0.1);
   textSize(20);
-  text('Lolipop', width*0.5, height*0.3);
-
-
-  if (drawStart == false){
-    background(255, 255, 255);
-    drawStart = true;
-  }
-  myButton.width = 90;
+  text('Lollipop', width*0.5, height*0.16);
+  myButton.width = 200;
   myButton.height = 70;
-  myButton.locate(380, 380);
+  myButton.locate(width*0.38, height*0.8);
   myButton.text = "Lvl4";
   myButton.color = "#00FF00";
   myButton.textSize = 30;
@@ -315,46 +278,44 @@ function play2() {
   myButton.onPress = function(){
    gamestate = "lvl4";
 }
-}
+
+  }
+
+
 
 function play3() {
-
   noStroke();
-  fill(245, 245, 220)
-  rect(0, 0, 500, 40, 20);
-  rect(0, 460, 500, 40, 20);
-  rect(0, 0, 20, 500, 20);
-  rect(480, 0, 20, 500, 20);
   fill(58);
-  rect(20, 40, 200, 420);
-  rect(20, 40, 440, 150);
-  rect(460, 40, 20, 420);
-  rect(20, 372, 460, 90);
+  rect(20, 40, 190, 790);
+  rect(20, 40, 790, 300);
+  rect(600, 40, 200, 800);
+  rect(20, 550, 800, 700);
+  fill(245, 300, 220);
+  fill(245, 245, 220);
+  strokeWeight(1);
+  rect(0, 0, 800, 40);
+  rect(0, 760, 800, 40);
+  rect(0, 0, 20, 800);
+  rect(780, 0, 20, 800);
+  textSize(40);
+  textAlign(CENTER);
+  text('Dell', width*0.5, height*.99);
   fill(0, 255, 0);
-  ellipse(400, 480, 20, 20);
+  ellipse(700, 780, 20, 20);
   fill(245, 245, 220);
   stroke(1);
-  ellipse(450, 480, 30, 30);
+  ellipse(750, 780, 30, 30);
   fill(58, 58, 58);
-  ellipse(450, 480, 10, 15);
-
-  image(pic0,45, 197);
-
+  ellipse(750, 780, 10, 15);
   fill(255, 255, 255);
   textAlign(CENTER);
   textSize(25);
-  text('Can you Draw:', width*0.5, height*0.2);
+  text('Can you Draw:', width*0.5, height*0.1);
   textSize(20);
-  text('Spider', width*0.5, height*0.3);
-
-
-  if (drawStart == false){
-    background(255, 255, 255);
-    drawStart = true;
-  }
-  myButton.width = 90;
+  text('Spider', width*0.5, height*0.16);
+  myButton.width = 200;
   myButton.height = 70;
-  myButton.locate(380, 380);
+  myButton.locate(width*0.38, height*0.8);
   myButton.text = "lvl5";
   myButton.color = "#00FF00";
   myButton.textSize = 30;
@@ -362,46 +323,43 @@ function play3() {
   myButton.onPress = function(){
    gamestate = "lvl5";
 }
-}
+  }
+
+
 
 function play4() {
-
   noStroke();
-  fill(245, 245, 220)
-  rect(0, 0, 500, 40, 20);
-  rect(0, 460, 500, 40, 20);
-  rect(0, 0, 20, 500, 20);
-  rect(480, 0, 20, 500, 20);
   fill(58);
-  rect(20, 40, 200, 420);
-  rect(20, 40, 440, 150);
-  rect(460, 40, 20, 420);
-  rect(20, 372, 460, 90);
+  rect(20, 40, 190, 790);
+  rect(20, 40, 790, 300);
+  rect(600, 40, 200, 800);
+  rect(20, 550, 800, 700);
+  fill(245, 300, 220);
+  fill(245, 245, 220);
+  strokeWeight(1);
+  rect(0, 0, 800, 40);
+  rect(0, 760, 800, 40);
+  rect(0, 0, 20, 800);
+  rect(780, 0, 20, 800);
+  textSize(40);
+  textAlign(CENTER);
+  text('Dell', width*0.5, height*.99);
   fill(0, 255, 0);
-  ellipse(400, 480, 20, 20);
+  ellipse(700, 780, 20, 20);
   fill(245, 245, 220);
   stroke(1);
-  ellipse(450, 480, 30, 30);
+  ellipse(750, 780, 30, 30);
   fill(58, 58, 58);
-  ellipse(450, 480, 10, 15);
-
-  image(pic0,45, 197);
-
+  ellipse(750, 780, 10, 15);
   fill(255, 255, 255);
   textAlign(CENTER);
   textSize(25);
-  text('Can you Draw:', width*0.5, height*0.2);
+  text('Can you Draw:', width*0.5, height*0.1);
   textSize(20);
-  text('Tree', width*0.5, height*0.3);
-
-
-  if (drawStart == false){
-    background(255, 255, 255);
-    drawStart = true;
-  }
-  myButton.width = 90;
+  text('ButterFly', width*0.5, height*0.16);
+  myButton.width = 200;
   myButton.height = 70;
-  myButton.locate(380, 380);
+  myButton.locate(width*0.38, height*0.8);
   myButton.text = "lvl6";
   myButton.color = "#00FF00";
   myButton.textSize = 30;
@@ -412,43 +370,38 @@ function play4() {
 }
 
 function play5() {
-
   noStroke();
-  fill(245, 245, 220)
-  rect(0, 0, 500, 40, 20);
-  rect(0, 460, 500, 40, 20);
-  rect(0, 0, 20, 500, 20);
-  rect(480, 0, 20, 500, 20);
   fill(58);
-  rect(20, 40, 200, 420);
-  rect(20, 40, 440, 150);
-  rect(460, 40, 20, 420);
-  rect(20, 372, 460, 90);
+  rect(20, 40, 190, 790);
+  rect(20, 40, 790, 300);
+  rect(600, 40, 200, 800);
+  rect(20, 550, 800, 700);
+  fill(245, 300, 220);
+  fill(245, 245, 220);
+  strokeWeight(1);
+  rect(0, 0, 800, 40);
+  rect(0, 760, 800, 40);
+  rect(0, 0, 20, 800);
+  rect(780, 0, 20, 800);
+  textSize(40);
+  textAlign(CENTER);
+  text('Dell', width*0.5, height*.99);
   fill(0, 255, 0);
-  ellipse(400, 480, 20, 20);
+  ellipse(700, 780, 20, 20);
   fill(245, 245, 220);
   stroke(1);
-  ellipse(450, 480, 30, 30);
+  ellipse(750, 780, 30, 30);
   fill(58, 58, 58);
-  ellipse(450, 480, 10, 15);
-
-  image(pic0,45, 197);
-
+  ellipse(750, 780, 10, 15);
   fill(255, 255, 255);
   textAlign(CENTER);
   textSize(25);
-  text('Can you Draw:', width*0.5, height*0.2);
+  text('Can you Draw:', width*0.5, height*0.1);
   textSize(20);
-  text('Head', width*0.5, height*0.3);
-
-
-  if (drawStart == false){
-    background(255, 255, 255);
-    drawStart = true;
-  }
-  myButton.width = 90;
+  text('Head', width*0.5, height*0.16);
+  myButton.width = 200;
   myButton.height = 70;
-  myButton.locate(380, 380);
+  myButton.locate(width*0.38, height*0.8);
   myButton.text = "lvl7";
   myButton.color = "#00FF00";
   myButton.textSize = 30;
@@ -456,46 +409,43 @@ function play5() {
   myButton.onPress = function(){
    gamestate = "lvl7";
 }
-}
+  }
+
+
 
 function play6() {
-
   noStroke();
-  fill(245, 245, 220)
-  rect(0, 0, 500, 40, 20);
-  rect(0, 460, 500, 40, 20);
-  rect(0, 0, 20, 500, 20);
-  rect(480, 0, 20, 500, 20);
   fill(58);
-  rect(20, 40, 200, 420);
-  rect(20, 40, 440, 150);
-  rect(460, 40, 20, 420);
-  rect(20, 372, 460, 90);
+  rect(20, 40, 190, 790);
+  rect(20, 40, 790, 300);
+  rect(600, 40, 200, 800);
+  rect(20, 550, 800, 700);
+  fill(245, 300, 220);
+  fill(245, 245, 220);
+  strokeWeight(1);
+  rect(0, 0, 800, 40);
+  rect(0, 760, 800, 40);
+  rect(0, 0, 20, 800);
+  rect(780, 0, 20, 800);
+  textSize(40);
+  textAlign(CENTER);
+  text('Dell', width*0.5, height*.99);
   fill(0, 255, 0);
-  ellipse(400, 480, 20, 20);
+  ellipse(700, 780, 20, 20);
   fill(245, 245, 220);
   stroke(1);
-  ellipse(450, 480, 30, 30);
+  ellipse(750, 780, 30, 30);
   fill(58, 58, 58);
-  ellipse(450, 480, 10, 15);
-
-  image(pic0,45, 197);
-
+  ellipse(750, 780, 10, 15);
   fill(255, 255, 255);
   textAlign(CENTER);
   textSize(25);
-  text('Can you Draw:', width*0.5, height*0.2);
+  text('Can you Draw:', width*0.5, height*0.1);
   textSize(20);
-  text('', width*0.5, height*0.3);
-
-
-  if (drawStart == false){
-    background(255, 255, 255);
-    drawStart = true;
-  }
-  myButton.width = 90;
+  text('Shoe', width*0.5, height*0.16);
+  myButton.width = 200;
   myButton.height = 70;
-  myButton.locate(380, 380);
+  myButton.locate(width*0.38, height*0.8);
   myButton.text = "End";
   myButton.color = "#00FF00";
   myButton.textSize = 30;
@@ -503,29 +453,37 @@ function play6() {
   myButton.onPress = function(){
    gamestate = "gameover";
 }
-}
+  }
+
+
 function gameOver() {
+  noStroke();
   fill(0,255,0);
-  rect(10, 20, 485, 475);
+  rect(0, 0, 800, 800);
   textAlign(CENTER);
   noStroke();
-  fill(245, 245, 220)
-  rect(0, 0, 500, 40, 20);
-  rect(0, 460, 500, 40, 20);
-  rect(0, 0, 20, 500, 20);
-  rect(480, 0, 20, 500, 20);
+  fill(245, 245, 220);
+  strokeWeight(1);
+  rect(0, 0, 800, 40);
+  rect(0, 760, 800, 40);
+  rect(0, 0, 20, 800);
+  rect(780, 0, 20, 800);
   stroke(255);
   fill(255);
   textSize(60);
   text('Results:', width*0.5, height*0.23);
   textSize(100);
   text('You Can!', width*0.5, height*0.55);
+  fill(245, 300, 220);
+  textSize(40);
+  textAlign(CENTER);
+  text('Dell', width*0.5, height*.99);
   fill(0, 255, 0);
-  ellipse(400, 480, 20, 20);
+  ellipse(700, 780, 20, 20);
   fill(245, 245, 220);
   stroke(1);
-  ellipse(450, 480, 30, 30);
+  ellipse(750, 780, 30, 30);
   fill(58, 58, 58);
-  ellipse(450, 480, 10, 15);
+  ellipse(750, 780, 10, 15);
   myButton.locate(-380, -380);
 }
